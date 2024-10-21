5e40f4bf1d83df16a1c2dd079d8f800dd91594e6
Tanzeela-coder <naztanzeela1122@gmail.com>
2024-10-21 01:24:47 -0700

    Add files via upload

---
+++ Assignment no.04, Tanzeela Naz , Batch-12 , WMA-325700/script.js
@@ -0,0 +1,45 @@
+// ---Assignment no. 04----
+// Qno.1
+
+//1.   Declare 3 variables in one statement. 
+
+// let a, b, c;
+
+// -------XXXXXXX-------
+
+// 2. Declare 5 legal & 5 illegal variable names. 
+
+
+// Legal Variable Names:
+// let firstName;
+// let _count;
+// let $price;
+// let totalAmount;
+// let user_age;
+
+// Illegal Variable Names:
+
+// let 1stName;  // – Cannot start with a number.
+// let user-age;  // – Hyphens are not allowed.
+// var ;           // – Cannot use reserved keywords (e.g., var, let, class).
+//let user age;       //– Spaces are not allowed in variable names.
+//let @value;   //– Special characters other than _ and $ are not allowed.
+
+// -------XXXXXXX-------
+// Qno.4
+// 3. Display this in your browser:
+//a)  A heading stating “Rules for naming JS variables” 
+//b)  Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or _____. For example $name, _name or name 
+//d) Variable names are case _________ 
+//e) Variable names should not be JS _________
+
+// ----show in browser----
+// document.write("<h1>Rules for naming JS variables</h1>  <br>");
+
+// document.write("Variable names can only contain firstName , count , $price  and  user_age.<br>");
+// document.write(" Variables must begin with a $name, _name or name. <br>");
+
+// document.write("Variable names are case Sensitive. <br>");
+// document.write("Variable names should not be JS Keyword.");
+// -----------xxxxxxxx----------
